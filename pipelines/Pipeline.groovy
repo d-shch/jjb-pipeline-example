dc96109de4f2f6214a47b1c6aa4a24a4c06d5306
Dmitry Shcherbakov <dshcherbakov@smartlabs.tv>
2018-12-11 14:56:55 +0300

    add lib to pipeline

--- pipelines/Pipeline.groovy
+++ pipelines/Pipeline.groovy
@@ -1,3 +1,5 @@
 node('master') {
+	MyLib.git("jjb-pipeline-example.git", "master")
+	sh 'ls'
 	sh "ping ${url} -c 5"
 }
--- vars/MyLib.groovy
+++ vars/MyLib.groovy
@@ -1,5 +1,5 @@
 def git(repo, branch) {
-	git(url: "${repo}",
+	git(url: "https://github.com/d-shch/${repo}",
 			credentialsId: "379268d2-8b01-42a1-bfa3-47eeee193be6",
 			branch: "${branch}")
 }
